effab4617aecd2947e366d577bb15f28ddce8228
Marc Summers <samthelegend68@gmail.com>
2026-09-06 18:15:31 +0100

    v0.0.5

--- Sources/MosaicKit/App/ServiceEnvironment.swift
+++ Sources/MosaicKit/App/ServiceEnvironment.swift
@@ -1,13 +1,18 @@
 import SwiftUI
 
 private struct AppServicesKey: EnvironmentKey {
-    static let defaultValue: AppServices = fatalError("AppServices not installed in environment")
+    static let defaultValue: AppServices? = nil
 }
 
 extension EnvironmentValues {
     /// The shared service bundle installed at the app root.
     var appServices: AppServices {
-        get { self[AppServicesKey.self] }
+        get {
+            guard let services = self[AppServicesKey.self] else {
+                fatalError("AppServices not installed in environment")
+            }
+            return services
+        }
         set { self[AppServicesKey.self] = newValue }
     }
 }
